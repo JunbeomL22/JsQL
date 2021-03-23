a0dac4d6f13356617b15c8c63b8f402863285635
Junbeom Lee <junbeoml22@gmail.com>
2021-03-23 20:52:41 +0900

    optimization example is compiled

--- examples/optimization_test.jl
+++ examples/optimization_test.jl
@@ -1,27 +1,27 @@
-using FiccPricer.Math
+using FiccPricer
 
-mutable struct TestCost <: CostFunction
+mutable struct TestCost <: FiccPricer.Math.CostFunction
 end
 
-function func_values(::TestCost, x::Vector{Float64}) 
-    y = zeros(undef, length(x))
-    for i = i:length(y)
+function FiccPricer.func_values(::TestCost, x::Vector{Float64}) 
+    y = zeros(Float64, length(x))
+    for i = 1:length(y)
         y[i] = (1.0-x[i])
     end
     return y
 end
 
-function value(::TestCost, x::Vector{Float64}) 
-    y = zeros(undef, length(x))
+function FiccPricer.value(::TestCost, x::Vector{Float64}) 
+    y = zeros(Float64, length(x))
     _value = 0.0
-    for i = i:length(y)
+    for i = 1:length(y)
         _value += (1.0-x[i])^2
     end
     return sqrt(_value)
 end
 
-p = Problem(TestCost(), NoConstraint(), [0.0, 0.0])
+p = FiccPricer.Math.Problem(TestCost(), FiccPricer.Math.NoConstraint(), [0.0, 0.0])
 
-om = LevenbergMarquardt()
-ec = EndCriteria(1000, 10, 1.0e-8, 1.0e-8, 1.0e-8)
-minimize!(om, p, ec)
+om = FiccPricer.Math.LevenbergMarquardt()
+ec = FiccPricer.Math.EndCriteria(1000, 10, 1.0e-8, 1.0e-8, 1.0e-8)
+FiccPricer.Math.minimize!(om, p, ec)
