1eabbd40e1989d7048198a3374b65b42c61a15cf
Junbeom Lee <junbeoml22@gmail.com>
2021-03-29 17:45:09 +0900

    example update

--- examples/svi_fit_test.jl
+++ examples/svi_fit_test.jl
@@ -11,32 +11,39 @@ strikes = [0.8, 0.9, 0.95, 0.975, 1.0, 1.025, 1.05, 1.1, 1.2]
 log_strikes = log.(strikes)
 vol1 = [34.28, 25.0, 20.75, 18.47, 16.15, 14.27, 13.2, 13.36, 18.49]
 vol2 = [32.55, 24.72, 21.02, 19.02, 17.02, 15.30, 14.12, 13.58, 16.53]
+
 # --- totla variance --- #
 total_variance1 = (vol1*0.01).^2.0 * t1
 total_variance1 = (vol2*0.01).^2.0 * t2
+
 # --- define cost function  --- #
 svi_cost1 = SviCost(strikes, vol1, t1, scale=0.01, isStrikeLog = false)
 svi_cost2 = SviCost(strikes, vol2, t2, scale=0.01, isStrikeLog = false)
+
 # --- define constraints  --- #
 butterfly_only = RawSviButterFlyConstraint()
 base_constraint = RawSviBaseConstraint()
 butterfly = FiccPricer.Math.JointConstraint(butterfly_only, base_constraint)
 calender = CalendarConstraint(log.(strikes), (0.01 * vol1).^2.0 * t1)
 joint_arbitrage_free = FiccPricer.Math.JointConstraint(butterfly, calender)
+
 # --- define initial values --- #
 initial_value = RawSviIntialValue().init
-initial_value = [min(total_variance1...), 0.01, 0.0, 0.0, 0.1]
+a_init = min(total_variance1...)
+initial_value = [0.1, 0.1, -0.01, 0.0, 0.1]
 # --- define problem and optimization method--- #
-p1 = FiccPricer.Math.Problem(svi_cost1, butterfly, initial_value)
-p1_noconstraint = FiccPricer.Math.Problem(svi_cost1, FiccPricer.Math.NoConstraint(), initial_value)
-#om = FiccPricer.Math.LevenbergMarquardt(1.0e-5, 1.0e-5, 1.0e-5, true)
-om = FiccPricer.Math.Simplex(0.0001)
-ec = FiccPricer.Math.EndCriteria(100000, 1000, 1.0e-8, 1.0e-8, 1.0e-8)
+p1 = FiccPricer.Math.Problem(svi_cost1, butterfly, copy(initial_value))
+p1_noconstraint = FiccPricer.Math.Problem(svi_cost1, FiccPricer.Math.NoConstraint(), copy(initial_value))
+
+#om = FiccPricer.Math.LevenbergMarquardt()#(1.0e-5, 1.0e-5, 1.0e-5, true)
+om = FiccPricer.Math.Simplex(0.01)
+ec = FiccPricer.Math.EndCriteria(1000, 10, 1.0e-8, 1.0e-8, 1.0e-8)
+
 # --- minimize ---#
 FiccPricer.Math.minimize!(om, p1, ec)
 FiccPricer.Math.minimize!(om, p1_noconstraint, ec)
-# --- results --- #
 
+# --- results --- #
 svi1 = RawSvi(p1.currentValue)
 svi1_noconstraint = RawSvi(p1_noconstraint.currentValue)
 
